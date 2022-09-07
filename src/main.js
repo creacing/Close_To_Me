5c37477bb04b4c10764212c51b8955dff2e372ce
123Nishiwei <shiwei.ni>
2022-09-07 14:08:10 +0800

    demo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { getPosts } from "@/utils/getPost.js";
 import '@/assets/app.css'
 import { createPinia } from 'pinia'
 
+getPosts()
 const app = createApp(App)
 
 app.use(router)
